e0fb7f2e9b4bdfb5449c855e7239554413ad696d
pieroolivares <piero-olivares@hotmail.com>
2022-06-28 21:40:41 +0200

    Añadido funcion y estilo de ver Posts y por texto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
+import PostsList from './Components/PostsList/PostsList';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/posts' element={<PostsList />} />
       </Routes>
     </div>
   );
---
+++ src/Components/PostsList/PostsList.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './Posts.css';
+import { useEffect, useState } from 'react';
+import { useToken } from '../../TokenContext';
+import { FaRegUser } from 'react-icons/fa';
+import Navbar from '../Navbar/Navbar';
+
+const PostsList = () => {
+  const [token] = useToken();
+  const [keyword, setKeyword] = useState('');
+
+  const [posts, setPosts] = useState(null);
+  const [error, setError] = useState(null);
+  const [update, setUpdate] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const getAllPosts = async () => {
+    setLoading(true);
+    setError(null);
+
+    const params = token
+      ? {
+          headers: {
+            Authorization: token,
+          },
+        }
+      : {};
+
+    try {
+      const response = await fetch(
+        `http://localhost:4000/posts?keyword=${keyword}`
+      );
+
+      const body = await response.json();
+      if (body.status === 'error') {
+        setPosts(null);
+        setError(body.message);
+        console.log(error);
+      } else {
+        setPosts(body.data.posts);
+      }
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    getAllPosts();
+  };
+
+  useEffect(() => {
+    getAllPosts();
+  }, [update]);
+
+  return (
+    <>
+      <Navbar />
+      <main className='MainPost'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='keyword'
+            placeholder=' Busca un post'
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <button disabled={loading}>Buscar</button>
+        </form>
+
+        {error && <p className='Error'>{error}</p>}
+
+        {posts && (
+          <ul className='PostList'>
+            {posts.map((post) => {
+              return (
+                <li key={post.id}>
+                  <header>
+                    <p>
+                      <FaRegUser />
+                      {post.username}
+                    </p>
+                  </header>
+                  <div>
+                    {post.image && (
+                      <img
+                        src={`http://localhost:4000/${post.image}`}
+                        alt='Imagen adjunta'
+                        sizes='470px'
+                      />
+                    )}
+                    <p>{post.text}</p>
+                  </div>
+                  <footer>
+                    <div className='like-section'>
+                      <p>♥ {post.likes}</p>
+                    </div>
+                  </footer>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </main>
+    </>
+  );
+};
+
+export default PostsList;
